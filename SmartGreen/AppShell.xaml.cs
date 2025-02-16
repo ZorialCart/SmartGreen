ac5f01ee7fc59644fa21cc4ffa4e65754e504f47
ZorialCart <zorialcart@outlook.com>
2025-02-15 17:28:38 -0700

    Flyout

--- SmartGreen/AppShell.xaml.cs
+++ SmartGreen/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace SmartGreen
+﻿using SmartGreen.View;
+using SmartGreen.View.ViveroView;
+namespace SmartGreen
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute("LoginPage", typeof(Login));
+            Routing.RegisterRoute("MenuView", typeof(MenuView));
+
         }
     }
 }
---
+++ SmartGreen/ViewModel/VMLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using SmartGreen.View;
+using SmartGreen.View.ViveroView;
+
+namespace SmartGreen.ViewModel
+{
+    internal class VMLogin : BaseViewModel
+    {
+        public VMLogin() { 
+           
+        
+        }
+
+        public async Task GoTo()
+        {
+            await Shell.Current.GoToAsync($"/{nameof(MenuView)}");
+        }
+        //Comandos
+        public ICommand ToMenu => new Command (async() => await GoTo());
+
+       
+    }
+}
