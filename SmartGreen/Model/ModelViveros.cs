da26e78afa3efbee1dc97c8315017a6ed80fb540
Alex5727 <alejandrogrjalva@gmail.com>
2025-03-19 12:14:57 -0700

    asd

--- SmartGreen/Model/ModelViveros.cs
+++ SmartGreen/Model/ModelViveros.cs
@@ -8,8 +8,17 @@ namespace SmartGreen.Model
 {
     public class ModelViveros
     {
-       
+
+        public string? id { get; set; }
+        public string? idInvernadero { get; set; }
+        public string? UsuCorreo { get; set; }
         public string? nombreInvernadero { get; set; }
-       
+        public int TipoInvernadero { get; set; }
+        public double MinHumedad { get; set; }
+        public double MaxHumedad { get; set; }
+        public double MinTemperatura { get; set; }
+        public double MaxTemperatura { get; set; }
+        public bool Started { get; set; }
+
     }
 }
