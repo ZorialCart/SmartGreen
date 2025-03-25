bf84783d50826e42c231a832308056300640f2eb
xCorick <corickgonzalez123@gmail.com>
2025-03-25 10:57:01 -0700

    Configuracion SignalR

---
+++ SmartGreen/Services/SignalRService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using SmartGreen.Model;
+using System.Threading.Tasks;
+
+namespace SmartGreen.Services
+{
+    public class SignalRService
+    {
+        private readonly HubConnection _hubconnection;
+        private InverStatusModel _lastInverStatus;
+
+        public event Action<InverStatusModel> OnStatusReceived;
+
+        public SignalRService()
+        {
+            _hubconnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5062/inverStatusHub")
+                .WithAutomaticReconnect()
+                .Build();
+        }
+
+        public async Task ConnectAsync(string IdInvernadero)
+        {
+            _hubconnection.On<InverStatusModel>("ReceiveStatus", (status) => 
+            {
+                if(status.idInvernadero == IdInvernadero)
+                {
+                    _lastInverStatus = status;
+                    MainThread.BeginInvokeOnMainThread(() =>
+                    {
+                        OnStatusReceived?.Invoke(_lastInverStatus);
+                    });
+                }
+            });
+
+            await _hubconnection.StartAsync();
+        }
+
+        public InverStatusModel GetLastStatus()
+        {
+            return _lastInverStatus;
+        }
+    }
+}
